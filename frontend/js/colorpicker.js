05df4da8e01b500988b35c62753d21409d0906bd
aravind <aravindreddy1572@gmail.com>
2021-05-05 18:53:19 +0530

    cp

---
+++ frontend/js/colorpicker.js
@@ -0,0 +1,7 @@
+function color()
+{
+    var x=document.getElementsByName("red")[0].value;
+    var y=document.getElementsByName("green")[0].value;
+    var z=document.getElementsByName("blue")[0].value;
+    alert(x+y+z);
+}

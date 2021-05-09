4b015bc4c871c8fef0043d38cc5d93c2b7cc8a5f
aravind <aravindreddy1572@gmail.com>
2021-05-09 20:57:51 +0530

    added chart

--- server.js
+++ server.js
@@ -11,22 +11,30 @@ app.get("/resume", function(req, res){
     let path = __dirname+"/frontend/html/myresume.html";
     res.sendFile(path);
 })
+app.get("/login", function(req, res){
+    let path = __dirname+"/frontend/html/login.html";
+    res.sendFile(path);
+})
+
 app.get("/colorpicker", function(req, res){
     let path = __dirname+"/frontend/html/colorpicker.html";
     res.sendFile(path);
 })
-app.get("/apple", function(req, res){
-    let path = __dirname+"/frontend/html/apple.html";
-    res.sendFile(path);
-})
-    app.get("/google", function(req, res){
+
+app.get("/google", function(req, res){
         let path = __dirname+"/frontend/html/google.html";
         res.sendFile(path);
 })
-// Heroku will automatically set an environment variable called PORT
-const PORT = process.env.PORT || 3000;
+app.get("/chart", function(req, res){
+    let path = __dirname+"/frontend/html/chart.html";
+    res.sendFile(path);
+})
+app.get("/reg", function(req, res){
+    let path = __dirname+"/frontend/html/registration.html";
+    res.sendFile(path);
+})
 
-// Start the server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
     console.log("Server Starting running on http://localhost:"+PORT);
 })
